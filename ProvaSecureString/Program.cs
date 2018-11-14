cb91a1d50ad44d843a76fe8a8f6f61c47bd1ce10
carlesalonso <carlos.martinez@mataro.epiaedu.cat>
2018-11-14 14:08:53 +0100

    Exemple us Secure String

---
+++ ProvaSecureString/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace ProvaSecureString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the secure string.
+            SecureString securePwd = new SecureString();
+            ConsoleKeyInfo key;
+
+            Console.Write("Enter password: ");
+            do
+            {
+                key = Console.ReadKey(true);
+
+                // Ignore any key out of range.
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                {
+                    // Append the character to the password.
+                    securePwd.AppendChar(key.KeyChar);
+                    Console.Write("*");
+                }
+                else if (key.Key == ConsoleKey.Backspace)
+                {
+                    Console.Write("\b \b");
+
+                }
+                // Exit if Enter key is pressed.
+            } while (key.Key != ConsoleKey.Enter);
+            Console.WriteLine();
+            writePassword(securePwd);
+            Console.ReadKey();
+
+
+
+
+        }
+
+        static void writePassword (SecureString secureString)
+        {
+            IntPtr bstr = Marshal.SecureStringToBSTR(secureString);
+
+            Console.WriteLine(Marshal.PtrToStringBSTR(bstr));
+
+            Marshal.ZeroFreeBSTR(bstr);
+        }
+    }
+}
